9754b0eb4c9ae4c909e1ec40f3ef8a9340322ae4
Karthikeya0612 <uppukarthikeya2002@gmail.com>
2023-06-05 22:58:20 +0530

    final code

--- myapp/views.py
+++ myapp/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
 import requests
-import datetime
-import logging
-logger = logging.getLogger(__name__)
 
-def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url):
+def fetch_weather_and_forecast(city, api_key, current_weather_url):
     current_response = requests.get(current_weather_url.format(city, api_key)).json()
-    logger.debug('API current_response: %s', current_response) 
-    lat, lon = current_response['coord']['lat'], current_response['coord']['lon']
-    forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json()
-    logger.debug("API response :%s", forecast_response)
+    # logger.debug('API current_response: %s', current_response) 
 
     weather_data = {
         'city': city,
@@ -18,44 +12,31 @@ def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url)
         'icon': current_response['weather'][0]['icon'],
     }
 
-    daily_forecasts = []
-    daily_data = forecast_response.get('daily', [])
-    for daily_data in daily_data:
-        daily_forecasts.append({
-            'day': datetime.datetime.fromtimestamp(daily_data['dt']).strftime('%A'),
-            'min_temp': round(daily_data['temp']['min'] - 273.15, 2),
-            'max_temp': round(daily_data['temp']['max'] - 273.15, 2),
-            'description': daily_data['weather'][0]['description'],
-            'icon': daily_data['weather'][0]['icon'],
-        })
 
-    return weather_data, daily_forecasts
+    return weather_data
 
 
 
 
 def index(request):
-    api_key = '1754d5cc1580e244fab3fc30f7a5824e'
+    api_key = '<API KEY>'
     current_weather_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
-    forecast_url = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}'
 
 
     if request.method == 'POST':
         city1 = request.POST['city1']
         city2 = request.POST.get('city2', None)
 
-        weather_data1, daily_forecasts1 = fetch_weather_and_forecast(city1, api_key, current_weather_url, forecast_url)
+        weather_data1= fetch_weather_and_forecast(city1, api_key, current_weather_url)
 
         if city2:
-            weather_data2, daily_forecasts2 = fetch_weather_and_forecast(city2, api_key, current_weather_url, forecast_url)
+            weather_data2 = fetch_weather_and_forecast(city2, api_key, current_weather_url)
         else:
-            weather_data2, daily_forecasts2 = None, None
+            weather_data2 = None
 
         context = {
             'weather_data1': weather_data1,
-            'daily_forecasts1': daily_forecasts1,
             'weather_data2': weather_data2,
-            'daily_forecasts2': daily_forecasts2,
         }
 
         return render(request, 'index.html', context)
